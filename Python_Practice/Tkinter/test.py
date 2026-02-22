9da33c1dae06f93c67b8b984810cfaf2bac1555e
Likhitha mahalaxmi <107251362+lily4412@users.noreply.github.com>
2026-02-22 19:54:32 +0530

    Add files via upload

---
+++ Python_Practice/Tkinter/test.py
@@ -0,0 +1,14 @@
+from tkinter import * 
+
+root = Tk()
+
+#Creating a Label Widget
+myLabel1 = Label(root, text = "Hello World!")
+myLabel2 = Label(root, text = "My name is Lily!")
+
+#Shoving it onto the screen
+myLabel1.grid(row = 0 , column = 0)
+myLabel2.grid(row = 1 , column = 5)
+
+root.mainloop()
+
