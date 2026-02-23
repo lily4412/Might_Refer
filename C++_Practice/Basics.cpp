aab1eb53a8c70a208b734fd265270cdf6038f714
Likhitha mahalaxmi <107251362+lily4412@users.noreply.github.com>
2026-02-23 21:31:25 +0530

    Add files via upload

---
+++ C++_Practice/Basics.cpp
@@ -0,0 +1,8 @@
+#include <iostream>  // Make sure this line is present
+
+using namespace std; // This allows using cout without std::
+
+int main() {
+    cout << "Hello, World!" << endl; // Should work now
+    return 0;
+}
